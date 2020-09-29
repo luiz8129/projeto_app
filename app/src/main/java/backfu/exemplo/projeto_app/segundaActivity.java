088903fcaf5c95544fddf0b6ff431c5d679c2fa9
luiz8129 <luizinhospider@hotmail.com>
2020-09-29 12:02:35 -0300

    Initial commit

---
+++ app/src/main/java/backfu/exemplo/projeto_app/segundaActivity.java
@@ -0,0 +1,14 @@
+package backfu.exemplo.projeto_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class segundaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
+}
